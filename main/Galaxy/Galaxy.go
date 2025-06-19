38e88cc6bb0916654c223c40400ff44079122a03
vampiloup <github@vampiloup.com>
2025-06-19 20:14:56 +0200

    initial commit

---
+++ main/Galaxy/Galaxy.go
@@ -0,0 +1,8 @@
+components {
+  id: "Galaxy"
+  component: "/main/Galaxy/Galaxy.script"
+}
+components {
+  id: "StarSystem"
+  component: "/main/Galaxy/StarSystem.factory"
+}
---
+++ main/Galaxy/StarSystem.go
@@ -0,0 +1,4 @@
+components {
+  id: "StarSystem"
+  component: "/main/Galaxy/StarSystem.script"
+}
